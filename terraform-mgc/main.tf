8b7ecae1ec666084566e9764065e4495c2e54a67
MichelH7cker <michel.hecker@usp.br>
2024-10-31 21:56:00 -0300

    feat: config front and back terraform

--- terraform-mgc/main.tf
+++ terraform-mgc/main.tf
@@ -1,5 +1,5 @@
-resource "mgc_virtual_machine_instances" "basic_instance" {
-  name     = "basic-instance"
+resource "mgc_virtual_machine_instances" "nexgen-front" {
+  name     = "nexgen-front"
   machine_type = {
     name = "cloud-bs1.xsmall"
   }
@@ -7,7 +7,23 @@ resource "mgc_virtual_machine_instances" "basic_instance" {
     name = "cloud-ubuntu-22.04 LTS"
   }
   network = {
-    associate_public_ip = false # If true, will create a public IP
+    associate_public_ip = true # If true, will create a public IP
+    delete_public_ip    = false
+  }
+
+  ssh_key_name = "michel"
+}
+
+resource "mgc_virtual_machine_instances" "nexgen-back" {
+  name     = "nexgen-back"
+  machine_type = {
+    name = "cloud-bs1.xsmall"
+  }
+  image = {
+    name = "cloud-ubuntu-22.04 LTS"
+  }
+  network = {
+    associate_public_ip = true # If true, will create a public IP
     delete_public_ip    = false
   }
 
