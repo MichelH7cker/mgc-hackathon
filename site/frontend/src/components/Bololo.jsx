6aae7400f883e763535f2816add937ae6d11fbb9
iTzFabregas <fabricio-sampaio@usp.br>
2024-10-31 23:51:10 -0300

    feat: mgalu 2

--- site/frontend/src/components/Bololo.jsx
+++ site/frontend/src/components/Bololo.jsx
@@ -3,7 +3,19 @@ export default function Bololo(info) {
 
     return (
         <>
-        <h1>OI</h1>
+        <div className="h-screen bg-gray-300 flex items-center justify-center">
+            <div className="bg-gray-200 w-9/12 h-5/6 flex shadow-2xl">
+                <div className="w-1/3 flex flex-col gap-20 text-center items-center">
+                    <h1 className="font-poppins font-black text-7xl mt-40">Forms</h1>
+                    <input type="text" className="bg-blue-500 w-96 h-16 rounded-2xl"/>
+                    <input type="text" className="bg-blue-500 w-96 h-16 rounded-2xl"/>
+                    <button className="bg-purple-500 w-96 h-16 rounded-2xl text-4xl font-poppins text-white font-black">Entrar</button>
+                </div>
+                <div className="w-2/3 flex items-center justify-center border-l-2">
+                    <img src="./magalu.png" alt="Magalu" className="w-5/6"/>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
